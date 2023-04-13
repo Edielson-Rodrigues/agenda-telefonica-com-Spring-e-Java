3f3e531f4395ec53aeeca619c7f9fb61b9e49b2c
Edielson Rodrigues <45544303+Edielson-Rodrigues@users.noreply.github.com>
2023-04-13 20:52:54 -0300

    Atualização DATA

--- agendaTelefonica/src/main/java/com/agendaTelefonica/data/DataConfiguration.java
+++ agendaTelefonica/src/main/java/com/agendaTelefonica/data/DataConfiguration.java
@@ -20,7 +20,7 @@ public class DataConfiguration {
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/agendaTelefonica");
 		dataSource.setUsername("root");
-		dataSource.setPassword("naruto5566");
+		dataSource.setPassword("password");
 		return dataSource;
 	}
 	
